d9241582393b572e32abd46be24b08fcf9e06d80
Valadian <berger_jesse@yahoo.com>
2017-07-01 22:56:10 -0500

    fixed broken terrain count in source util

--- src/util.source.ts
+++ src/util.source.ts
@@ -9,26 +9,27 @@
 var DIRS = [{x:0,y:-1},{x:1,y:-1},{x:0,y:1},{x:1,y:1},{x:0,y:1},{x:-1,y:1},{x:-1,y:0},{x:-1,y:-1}]
 export function findsourceid(creep:Creep): string{
     var sources = creep.room.find(FIND_SOURCES) as Source[];
-    sources.sort((a,b)=> b.energy - a.energy);
-    return sources[0].id;
-    // var source_ratios = {}
-    // for(var source of sources){
-    //     if(!Memory.source_harvest_slots){
-    //         Memory.source_harvest_slots = {}
-    //     }
-    //     if(!Memory.source_harvest_slots[source.id]){
-    //         var terrain = creep.room.lookForAtArea(LOOK_TERRAIN,source.pos.y-1,source.pos.x-1,source.pos.y+1,source.pos.x+1,true) as LookAtResultWithPos[];
-    //         Memory.source_harvest_slots[source.id]=9-terrain.length;
-    //     }
-    //     var results = creep.room.lookForAtArea(LOOK_CREEPS,source.pos.y-1,source.pos.x-1,source.pos.y+1,source.pos.x+1,true) as LookAtResultWithPos[];
-    //     var num_harvesting = results.map((result) => result.creep).filter((creep:Creep) => creep.memory.mode=="HARVEST").length;
-    //     source_ratios[source.id] = num_harvesting/Memory.source_harvest_slots[source.id];
-    // }
-    // var items = Object.keys(source_ratios).map(function(key) {
-    //     return [key, source_ratios[key]];
-    // });
-    // items.sort(function(first, second) {
-    //     return second[1] - first[1];
-    // });
-    // return items[0][0];
+    // sources.sort((a,b)=> b.energy - a.energy);
+    // return sources[0].id;
+    var source_ratios = {}
+    for(var source of sources){
+        if(!Memory.source_harvest_slots){
+            Memory.source_harvest_slots = {}
+        }
+        if(!Memory.source_harvest_slots[source.id]){
+            var terrain = creep.room.lookForAtArea(LOOK_TERRAIN,source.pos.y-1,source.pos.x-1,source.pos.y+1,source.pos.x+1,true) as LookAtResultWithPos[];
+            terrain = terrain.filter((t)=>t.terrain=="wall")
+            Memory.source_harvest_slots[source.id]=9-terrain.length;
+        }
+        var results = creep.room.lookForAtArea(LOOK_CREEPS,source.pos.y-1,source.pos.x-1,source.pos.y+1,source.pos.x+1,true) as LookAtResultWithPos[];
+        var num_harvesting = results.map((result) => result.creep).filter((creep:Creep) => creep.memory.mode=="HARVEST").length;
+        source_ratios[source.id] = num_harvesting/Memory.source_harvest_slots[source.id];
+    }
+    var items = Object.keys(source_ratios).map(function(key) {
+        return [key, source_ratios[key]];
+    });
+    items.sort(function(first, second) {
+        return second[1] - first[1];
+    });
+    return items[0][0];
 }
