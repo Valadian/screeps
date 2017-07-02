1e7e36271cbd7d4a82dd0a96911b2e759339dfce
Valadian <berger_jesse@yahoo.com>
2017-07-02 13:05:22 -0500

    temporary spawn rule for 1250 cap

--- dev/main.js
+++ dev/main.js
@@ -73,6 +73,9 @@ function spawnNewCreeps(spawnName) {
         else if (checkThenSpawn(spawnName, 'upgrader', 6, L4_1300_Worker, energy)) { }
     }
     else if (Game.spawns[spawnName].room.energyCapacityAvailable >= 800) {
+        checkThenSpawn(spawnName, 'harvester', 6, [MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY], energy);
+    }
+    if (Game.spawns[spawnName].room.energyCapacityAvailable >= 800) {
         if (checkThenSpawn(spawnName, 'harvester', 3, L3_800_Worker, energy)) { }
         else if (checkThenSpawn(spawnName, 'upgrader', 1, L3_800_Worker, energy)) { }
         else if (checkThenSpawn(spawnName, 'harvester', 3, L3_800_Worker, energy)) { }
