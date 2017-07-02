86c4ac9f3787646a52f8fefdcfc8e15fc03f07ab
Valadian <berger_jesse@yahoo.com>
2017-07-02 15:30:17 -0500

    rewriting spawn role

--- dev/role.spawn.js
+++ dev/role.spawn.js
@@ -1,17 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function run(creep) {
-    for (var name in Game.flags) {
-        var flag = Game.flags[name];
-        if (flag.room == undefined) {
-            creep.travelTo(flag);
+    if (!creep.room.controller.my) {
+        if (creep.claimController(flag.room.controller) == ERR_NOT_IN_RANGE) {
+            creep.travelTo(flag.room.controller);
         }
-        else {
-            if (!flag.room.controller.my) {
-                if (creep.claimController(flag.room.controller) == ERR_NOT_IN_RANGE) {
-                    creep.travelTo(flag.room.controller);
-                }
-                break;
+    }
+    else {
+        for (var name in Game.flags) {
+            var flag = Game.flags[name];
+            if (flag.room == undefined) {
+                creep.travelTo(flag);
             }
         }
     }
--- src/role.spawn.ts
+++ src/role.spawn.ts
@@ -1,14 +1,13 @@
 export function run(creep:Creep) {
-    for(var name in Game.flags){
-        var flag = Game.flags[name];
-        if(flag.room == undefined){
-            creep.travelTo(flag)
-        } else {
-            if(!flag.room.controller.my){
-                if(creep.claimController(flag.room.controller) == ERR_NOT_IN_RANGE){
-                    creep.travelTo(flag.room.controller)
-                }
-                break;
+    if (!creep.room.controller.my){
+        if(creep.claimController(flag.room.controller) == ERR_NOT_IN_RANGE){
+            creep.travelTo(flag.room.controller)
+        }
+    } else {
+        for(var name in Game.flags){
+            var flag = Game.flags[name];
+            if(flag.room == undefined){
+                creep.travelTo(flag)
             }
         }
     }
