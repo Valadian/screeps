3ba86add05f83fbf1b6acfd0d590cf31cb586b74
Valadian <berger_jesse@yahoo.com>
2017-07-02 13:05:41 -0500

    typo for temporary rule

--- dev/main.js
+++ dev/main.js
@@ -72,7 +72,7 @@ function spawnNewCreeps(spawnName) {
         else if (checkThenSpawn(spawnName, 'harvester', 3, L4_1300_Worker, energy)) { }
         else if (checkThenSpawn(spawnName, 'upgrader', 6, L4_1300_Worker, energy)) { }
     }
-    else if (Game.spawns[spawnName].room.energyCapacityAvailable >= 800) {
+    else if (Game.spawns[spawnName].room.energyCapacityAvailable >= 1250) {
         checkThenSpawn(spawnName, 'harvester', 6, [MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY], energy);
     }
     if (Game.spawns[spawnName].room.energyCapacityAvailable >= 800) {
--- src/main.ts
+++ src/main.ts
@@ -74,7 +74,7 @@ function spawnNewCreeps(spawnName:string){
         else if(checkThenSpawn(spawnName,'harvester',3,L4_1300_Worker,energy)){}
         else if(checkThenSpawn(spawnName,'upgrader',6,L4_1300_Worker,energy)){}
     } else 
-    if (Game.spawns[spawnName].room.energyCapacityAvailable>=800){
+    if (Game.spawns[spawnName].room.energyCapacityAvailable>=1250){
         checkThenSpawn(spawnName,'harvester',6,[MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, WORK, CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY],energy)
     }
     if (Game.spawns[spawnName].room.energyCapacityAvailable>=800){
