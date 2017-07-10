73c405daf35f222468cadd2e9f60f97e6339ce52
Jesse Berger <jesse.berger@metecs.com>
2017-07-10 12:18:18 -0500

    remove claim logging

--- dev/role.spawn.js
+++ dev/role.spawn.js
@@ -3,18 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Claim {
     static run(creep) {
         for (var name of Object.keys(Game.flags)) {
-            console.log("Num flags: " + Object.keys(Game.flags).length);
             var isClaim = name.toLowerCase().startsWith("claim");
-            console.log(name + " is claim? " + isClaim);
             if (isClaim) {
-                console.log("found claim flag");
                 var flag = Game.flags[name];
                 if (flag.room == undefined) {
-                    console.log("moving to claim flag room");
                     creep.travelTo(flag, { useFindRoute: true, allowHostile: true, ensurePath: true, maxRooms: 4 });
                 }
                 else {
-                    console.log("claiming controller");
                     if (flag.room == creep.room && (creep.room.controller.my == undefined || creep.room.controller.my == false)) {
                         if (creep.claimController(flag.room.controller) == ERR_NOT_IN_RANGE) {
                             creep.travelTo(flag.room.controller, { useFindRoute: true, allowHostile: true, ensurePath: true, maxRooms: 0 });
--- src/role.spawn.ts
+++ src/role.spawn.ts
@@ -8,17 +8,17 @@ export default class Claim{
         //     }
         // } else {
             for(var name of Object.keys(Game.flags)){
-                console.log("Num flags: "+Object.keys(Game.flags).length)
+                //console.log("Num flags: "+Object.keys(Game.flags).length)
                 var isClaim = name.toLowerCase().startsWith("claim")
-                console.log(name+" is claim? "+isClaim)
+                //console.log(name+" is claim? "+isClaim)
                 if(isClaim){
-                    console.log("found claim flag")
+                    //console.log("found claim flag")
                     var flag = Game.flags[name];
                     if(flag.room == undefined){
-                        console.log("moving to claim flag room")
+                        //console.log("moving to claim flag room")
                         creep.travelTo(flag,{useFindRoute:true,allowHostile:true,ensurePath:true,maxRooms:4})
                     } else {
-                        console.log("claiming controller")
+                        //console.log("claiming controller")
                         if(flag.room == creep.room && (creep.room.controller.my==undefined || creep.room.controller.my ==false)){
                             if(creep.claimController(flag.room.controller)==ERR_NOT_IN_RANGE){
                                 creep.travelTo(flag.room.controller,{useFindRoute:true,allowHostile:true,ensurePath:true,maxRooms:0})
