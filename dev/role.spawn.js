d1e265827ea53a1a91572ab070ae9b1d4b465530
Jesse Berger <jesse.berger@metecs.com>
2017-07-10 12:17:44 -0500

    check for controller.my undefined or false

--- dev/role.spawn.js
+++ dev/role.spawn.js
@@ -15,7 +15,7 @@ class Claim {
                 }
                 else {
                     console.log("claiming controller");
-                    if (flag.room == creep.room && creep.room.controller.my == undefined) {
+                    if (flag.room == creep.room && (creep.room.controller.my == undefined || creep.room.controller.my == false)) {
                         if (creep.claimController(flag.room.controller) == ERR_NOT_IN_RANGE) {
                             creep.travelTo(flag.room.controller, { useFindRoute: true, allowHostile: true, ensurePath: true, maxRooms: 0 });
                         }
