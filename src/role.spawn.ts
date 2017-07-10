8e14f82d2a289014c84d440934b5541f4fd35d09
Jesse Berger <jesse.berger@metecs.com>
2017-07-10 11:00:11 -0500

    adding spawn logging

--- src/role.spawn.ts
+++ src/role.spawn.ts
@@ -8,11 +8,14 @@ export default class Claim{
         //     }
         // } else {
             for(var name in Object.keys(Game.flags)){
+                console.log("found claim flag")
                 if(name.toLowerCase().startsWith("claim")){
                     var flag = Game.flags[name];
                     if(flag.room == undefined){
-                        creep.travelTo(flag.pos)
+                        console.log("moving to claim flag room")
+                        creep.travelTo(flag)
                     } else {
+                        console.log("claiming controller")
                         if(flag.room == creep.room && creep.room.controller.my==undefined){
                             if(creep.claimController(creep.room.controller)==ERR_NOT_IN_RANGE){
                                 creep.travelTo(creep.room.controller)
