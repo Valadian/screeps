693a420af119cf32458472ae2e1e212f992a9cd9
Jesse Berger <jesse.berger@metecs.com>
2017-07-10 10:44:00 -0500

    improving claim

--- dev/role.spawn.js
+++ dev/role.spawn.js
@@ -2,25 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class Claim {
     static run(creep) {
-        if (creep.room.controller.my == undefined) {
-            var err = creep.claimController(creep.room.controller);
-            creep.say("" + err);
-            if (err == ERR_NOT_IN_RANGE) {
-                creep.travelTo(creep.room.controller);
-            }
-        }
-        else {
-            for (var name in Game.flags) {
-                if (name.startsWith("claim")) {
-                    var flag = Game.flags[name];
-                    if (flag.room == undefined) {
-                        creep.travelTo(flag);
-                    }
-                    else {
-                        if (flag.room == creep.room) {
-                            if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                                creep.travelTo(creep.room.controller);
-                            }
+        for (var name in Game.flags) {
+            if (name.startsWith("claim")) {
+                var flag = Game.flags[name];
+                if (flag.room == undefined) {
+                    creep.travelTo(flag);
+                }
+                else {
+                    if (flag.room == creep.room && creep.room.controller.my == undefined) {
+                        if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                            creep.travelTo(creep.room.controller);
                         }
                     }
                 }
--- src/role.spawn.ts
+++ src/role.spawn.ts
@@ -1,19 +1,19 @@
 export default class Claim{ 
     public static run(creep:Creep) {
-        if (creep.room.controller.my==undefined){
-            var err = creep.claimController(creep.room.controller)
-            creep.say(""+err);
-            if(err == ERR_NOT_IN_RANGE){
-                creep.travelTo(creep.room.controller);
-            }
-        } else {
+        // if (creep.room.controller.my==undefined){
+        //     var err = creep.claimController(creep.room.controller)
+        //     creep.say(""+err);
+        //     if(err == ERR_NOT_IN_RANGE){
+        //         creep.travelTo(creep.room.controller);
+        //     }
+        // } else {
             for(var name in Game.flags){
                 if(name.startsWith("claim")){
                     var flag = Game.flags[name];
                     if(flag.room == undefined){
                         creep.travelTo(flag)
                     } else {
-                        if(flag.room == creep.room){
+                        if(flag.room == creep.room && creep.room.controller.my==undefined){
                             if(creep.claimController(creep.room.controller)==ERR_NOT_IN_RANGE){
                                 creep.travelTo(creep.room.controller)
                             }
@@ -21,7 +21,7 @@ export default class Claim{
                     }
                 }
             }
-        }
+        // }
         //var flag = creep.pos.findClosestByPath(FIND_FLAGS) as Flag;
     }
 }
